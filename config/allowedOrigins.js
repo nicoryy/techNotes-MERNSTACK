892fdfc41d3dd4c11b7177d4b155fa4ec38e5771
nicoryy <pedronicory@gmail.com>
2024-05-01 18:22:28 -0300

    lesson 2

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://l3solution.net.br",
+  "https://llassessoriaeservicos.com.br",
+  "https://www.l3solution.net.br",
+  "https://www.llassessoriaeservicos.com.br",
+];
+
+module.exports = allowedOrigins
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const path = require("path");
 const env = require("dotenv");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+
+const app = express();
 
 env.config();
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(cors(corsOptions))
+app.use(logger);
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
@@ -19,5 +29,7 @@ app.use("*", (req, res) => {
     res.send({ message: "404 Not Found" });
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`));
